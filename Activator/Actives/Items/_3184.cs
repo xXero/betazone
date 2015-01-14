e454f938f0a8299e3c8f9ea2a40bce43d101a4a2
legacy3 <legacy3@users.noreply.github.com>
2015-01-14 07:53:31 +0100

    Added Entropy

---
+++ Activator/Actives/Items/_3184.cs
@@ -0,0 +1,52 @@
+﻿#region
+
+using System.Collections.Generic;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Activator.Actives.Items
+{
+    internal class _3184 : Item
+    {
+        internal override int Id
+        {
+            get { return 3184; }
+        }
+
+        internal override List<Utility.Map.MapType> Maps
+        {
+            get { return new List<Utility.Map.MapType> { Utility.Map.MapType.CrystalScar }; }
+        }
+
+        internal override string Name
+        {
+            get { return "Entropy"; }
+        }
+
+        internal override float Range
+        {
+            get { return Orbwalking.GetRealAutoAttackRange(null) + 65f; }
+        }
+
+        internal override ScalingType Scaling
+        {
+            get { return ScalingType.Ad; }
+        }
+
+        internal override ItemType Type
+        {
+            get { return ItemType.Offensive; }
+        }
+
+        internal override void Use()
+        {
+            var target = TargetSelector.GetTarget(Range, TargetSelector.DamageType.Physical);
+
+            if (target.IsValidTarget(Range))
+            {
+                Cast();
+            }
+        }
+    }
+}
