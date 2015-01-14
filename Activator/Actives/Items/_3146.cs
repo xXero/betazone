ccf9bb916afaba9524d44b98eb9c29a09fefdc20
legacy3 <legacy3@users.noreply.github.com>
2015-01-14 09:49:23 +0100

    Added Hextech Gunblade

---
+++ Activator/Actives/Items/_3146.cs
@@ -0,0 +1,61 @@
+﻿#region
+
+using System.Collections.Generic;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Activator.Actives.Items
+{
+    internal class _3146 : Item
+    {
+        internal override int Id
+        {
+            get { return 3146; }
+        }
+
+        internal override List<Utility.Map.MapType> Maps
+        {
+            get
+            {
+                return new List<Utility.Map.MapType>
+                {
+                    Utility.Map.MapType.CrystalScar,
+                    Utility.Map.MapType.HowlingAbyss,
+                    Utility.Map.MapType.SummonersRift,
+                    Utility.Map.MapType.TwistedTreeline
+                };
+            }
+        }
+
+        internal override string Name
+        {
+            get { return "Hextech Gunblade"; }
+        }
+
+        internal override float Range
+        {
+            get { return 700f; }
+        }
+
+        internal override ScalingType Scaling
+        {
+            get { return ScalingType.Ap; }
+        }
+
+        internal override ItemType Type
+        {
+            get { return ItemType.Offensive; }
+        }
+
+        internal override void Use()
+        {
+            var target = TargetSelector.GetTarget(Range, TargetSelector.DamageType.Magical);
+
+            if (target.IsValidTarget(Range))
+            {
+                Cast(target);
+            }
+        }
+    }
+}
