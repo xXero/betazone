beb82898cb68310d60455d26c1d440e6436627e9
legacy3 <legacy3@users.noreply.github.com>
2015-01-16 23:15:53 +0100

    moved the events to utils

--- Activator/Utils/Events/Stealth.cs
+++ Activator/Utils/Events/Stealth.cs
@@ -5,14 +5,13 @@ using LeagueSharp;
 
 #endregion
 
-namespace Activator.Actives.Events
+namespace Activator.Utils.Events
 {
     internal class Stealth
     {
         public delegate void OnStealthDelegate(Obj_AI_Hero unit);
 
-        //TODO: Add remaining spells to this when L# updates
-        public static string[] StealthSpells = {"khazixR"};
+        public static string[] StealthSpells = { "khazixR" };
 
         static Stealth()
         {
@@ -23,22 +22,19 @@ namespace Activator.Actives.Events
 
         private static void ObjAiBaseOnOnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            // Make sure sender is an enemy
             if (!sender.IsEnemy)
             {
                 return;
             }
 
-            // Check is spell is listed in the array above
             if (!StealthSpells.Any(x => x.Equals(args.SData.Name)))
             {
                 return;
             }
 
-            // Fire the event
             if (OnStealth != null)
             {
-                OnStealth(sender as Obj_AI_Hero);
+                OnStealth((Obj_AI_Hero) sender);
             }
         }
     }
