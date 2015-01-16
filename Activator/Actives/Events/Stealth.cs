0cb3f1b3a7a820b071fcd855449a6acc95774d09
Justin Perez <chewymoonleaguesharp@gmail.com>
2015-01-16 16:06:55 -0500

    Basic Anti Stealth ; w ;

---
+++ Activator/Actives/Events/Stealth.cs
@@ -0,0 +1,52 @@
+﻿#region
+
+using System.Linq;
+using LeagueSharp;
+
+#endregion
+
+namespace Activator.Actives.Events
+{
+    internal class Stealth
+    {
+        public delegate void OnStealthDelegate(Obj_AI_Hero unit);
+
+        //TODO: Add remaining spells to this when L# updates
+        public static string[] StealthSpells = {"khazixR"};
+
+        static Stealth()
+        {
+            Obj_AI_Base.OnProcessSpellCast += ObjAiBaseOnOnProcessSpellCast;
+        }
+
+        public static event OnStealthDelegate OnSealth;
+
+        private static void ObjAiBaseOnOnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            // Make sure sender is an enemy
+            if (!sender.IsEnemy)
+            {
+                return;
+            }
+
+            // See is sender is a hero
+            var hero = sender as Obj_AI_Hero;
+            if (hero == null)
+            {
+                return;
+            }
+
+            // Check is spell is listed in the array above
+            if (!StealthSpells.Any(x => x.Equals(args.SData.Name)))
+            {
+                return;
+            }
+
+            // Fire the event, is valid
+            if (OnSealth != null)
+            {
+                OnSealth(hero);
+            }
+        }
+    }
+}
