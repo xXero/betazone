948d7279da9588f457d85aee447dc6635f850347
DZ191 <darkprince97@live.it>
2015-01-20 18:53:34 +0100

    lol

--- Activator/Actives/Items/_3159.cs
+++ Activator/Actives/Items/_3159.cs
@@ -49,7 +49,7 @@ namespace Activator.Actives.Items
 
         internal override void Use()
         {
-            Stealth.OnStealth += sender => { Cast(sender.ServerPosition); };
+            Stealth.OnStealth += sender => Cast(sender.ServerPosition);
         }
     }
 }
