778bf650949132940369f2f207ddcc768e418781
DZ191 <darkprince97@live.it>
2015-01-20 17:31:20 +0100

    Randuin's Omen

---
+++ Activator/Actives/Items/_3143.cs
@@ -0,0 +1,59 @@
+﻿#region
+
+using System.Collections.Generic;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Activator.Actives.Items
+{
+    internal class _3143 : Item
+    {
+        internal override int Id
+        {
+            get { return 3143; }
+        }
+
+        internal override List<Utility.Map.MapType> Maps
+        {
+            get
+            {
+                return new List<Utility.Map.MapType>
+                {
+                    Utility.Map.MapType.CrystalScar,
+                    Utility.Map.MapType.HowlingAbyss,
+                    Utility.Map.MapType.SummonersRift,
+                    Utility.Map.MapType.TwistedTreeline
+                };
+            }
+        }
+
+        internal override string Name
+        {
+            get { return "Randuin's Omen"; }
+        }
+
+        internal override float Range
+        {
+            get { return 450f; }
+        }
+
+        internal override ScalingType Scaling
+        {
+            get { return ScalingType.Ad; }
+        }
+
+        internal override ItemType Type
+        {
+            get { return ItemType.Defensive; }
+        }
+
+        internal override void Use()
+        { 
+            if (Activator.Player.CountEnemiesInRange(Range) >= 2)
+            {
+                Cast();
+            }
+        }
+    }
+}
