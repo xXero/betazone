3e06c9e6d8042112c66b1da9e3de325212cc21bf
DZ191 <darkprince97@live.it>
2015-01-20 17:52:05 +0100

    Added Tiamat & Hydra

---
+++ Activator/Actives/Items/_3077.cs
@@ -0,0 +1,62 @@
+﻿#region
+
+using System.Collections.Generic;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Activator.Actives.Items
+{
+    internal class _3077 : Item
+    {
+        internal override int Id
+        {
+            get { return 3077; }
+        }
+
+        internal override List<Utility.Map.MapType> Maps
+        {
+            get
+            {
+                return new List<Utility.Map.MapType>
+                {
+                    Utility.Map.MapType.CrystalScar,
+                    Utility.Map.MapType.HowlingAbyss,
+                    Utility.Map.MapType.SummonersRift,
+                    Utility.Map.MapType.TwistedTreeline
+                };
+            }
+        }
+
+        internal override string Name
+        {
+            get { return "Tiamat"; }
+        }
+        internal override float Range
+        {
+            get { return 400f; }
+        }
+
+        internal override ScalingType Scaling
+        {
+            get { return ScalingType.Ad; }
+        }
+
+        internal override ItemType Type
+        {
+            get { return ItemType.Offensive; }
+        }
+
+        internal override void Construct()
+        {
+            SubMenu.AddItem(new MenuItem("minEnemiesAround", "Min. enemies around").SetValue(new Slider(1, 1, 5)));
+        }
+        internal override void Use()
+        {
+            if (Activator.Player.CountEnemiesInRange(Range) >= SubMenu.Item("minEnemiesAround").GetValue<Slider>().Value)
+            {
+                Cast();
+            }
+        }
+    }
+}
