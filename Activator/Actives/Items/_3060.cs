c6a8b91e27deb975c5dc112d671c22ddd03c5c4f
DZ191 <darkprince97@live.it>
2015-01-20 17:45:06 +0100

    Banner of Command Changes

--- Activator/Actives/Items/_3060.cs
+++ Activator/Actives/Items/_3060.cs
@@ -50,39 +50,16 @@ namespace Activator.Actives.Items
 
         internal override void Use()
         {
-            // find a target that is nearby
             var nearbyTarget = TargetSelector.GetTarget(Range, TargetSelector.DamageType.True);
             if (!nearbyTarget.IsValidTarget())
             {
                 return;
             }
-
-            // try to find a siege minion
-            var bestMinion =
-                ObjectManager.Get<Obj_AI_Minion>()
-                    .Where(x => x.Distance(Activator.Player) < Range)
-                    .Where(x => x.IsAlly)
-                    .FirstOrDefault(
-                        x => x.BaseSkinName == "SRU_OrderMinionSiege" || x.BaseSkinName == "SRU_ChaosMinionSiege");
-
+            var minionList = MinionManager.GetMinions(Range, MinionTypes.All, MinionTeam.Ally);
+            var bestMinion = minionList.MaxOrDefault(minion => !minion.BaseSkinName.Contains("Super"));
             if (bestMinion != null)
             {
                 LeagueSharp.Common.Items.UseItem(Id, bestMinion);
-                return;
-            }
-
-            // didn't find siege minion, find minion with most health that isn't a super minion
-            var nextMinion =
-                ObjectManager.Get<Obj_AI_Minion>()
-                    .Where(x => x.Distance(Activator.Player) < Range)
-                    .Where(x => x.IsAlly)
-                    .Where(x => x.BaseSkinName != "SRU_OrderMinionSuper" && x.BaseSkinName != "SRU_ChaosMinionSuper")
-                    .OrderBy(x => x.Health)
-                    .FirstOrDefault();
-
-            if (nextMinion != null)
-            {
-                LeagueSharp.Common.Items.UseItem(Id, nextMinion);
             }
         }
     }
