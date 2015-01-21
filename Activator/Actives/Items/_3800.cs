98f5b362f7ce7a73649741fc2668c549f058f35b
DZ191 <darkprince97@live.it>
2015-01-21 18:45:23 +0100

    Righteous Glory + TODO Update

---
+++ Activator/Actives/Items/_3800.cs
@@ -0,0 +1,64 @@
+﻿#region
+
+using System.Collections.Generic;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Activator.Actives.Items
+{
+    internal class _3800 : Item
+    {
+        internal override int Id
+        {
+            get { return 3800; }
+        }
+
+        internal override List<Utility.Map.MapType> Maps
+        {
+            get
+            {
+                return new List<Utility.Map.MapType>
+                {
+                    Utility.Map.MapType.CrystalScar,
+                    Utility.Map.MapType.HowlingAbyss,
+                    Utility.Map.MapType.SummonersRift,
+                    Utility.Map.MapType.TwistedTreeline
+                };
+            }
+        }
+
+        internal override string Name
+        {
+            get { return "Righteous Glory"; }
+        }
+
+        internal override float Range
+        {
+            //TODO Check this.
+            get { return 600f; }
+        }
+
+        internal override ScalingType Scaling
+        {
+            get { return ScalingType.Ad; }
+        }
+
+        internal override ItemType Type
+        {
+            get { return ItemType.Defensive; }
+        }
+
+        internal override void Construct()
+        {
+            SubMenu.AddItem(new MenuItem("minAlliesAround", "Min. allies around").SetValue(new Slider(1, 1, 5)));
+        }
+        internal override void Use()
+        {
+            if (Activator.Player.CountEnemiesInRange(Range) >= SubMenu.Item("minAlliesAround").GetValue<Slider>().Value)
+            {
+                Cast();
+            }
+        }
+    }
+}
